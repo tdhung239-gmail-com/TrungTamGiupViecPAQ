657d00cbbef3bd7503ceb9a9ec6003b55e8bb8e7
tdhung239 <tdhung239@gmail.com>
2021-05-24 16:38:47 +0700

    anh-tuan dich-vu

---
+++ ChamSocBenhNhan/src/main/java/ChamSocBenhNhan/Service/user/HomeUserIml.java
@@ -0,0 +1,68 @@
+package ChamSocBenhNhan.Service.user;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ChamSocBenhNhan.Dao.User.HomeUsserDao;
+import ChamSocBenhNhan.Entity.User.ChonDichVu;
+import ChamSocBenhNhan.Entity.User.LienHe;
+import ChamSocBenhNhan.Entity.User.ListDangKyDichVu;
+import ChamSocBenhNhan.Entity.User.ListNhanVienvaDichVu;
+import ChamSocBenhNhan.Entity.User.TuyenDung;
+
+@Service
+public class HomeUserIml implements HomeUserService { 
+	@Autowired
+	private HomeUsserDao listDao = new HomeUsserDao(); 
+
+	public List<ListNhanVienvaDichVu> getListCardE() {
+		return listDao.getListCardE();
+	}
+	public List<ListNhanVienvaDichVu> searchNameService(String tenDichVu) {
+		return listDao.searchNameService(tenDichVu);
+	}
+	
+	public List<ListDangKyDichVu> getTotalRegisterService() {
+		return listDao.getTotalRegisterService();
+	}
+
+	public List<ListNhanVienvaDichVu> getListService() { 
+		return listDao.getListService(); 
+	}
+	 
+	// tuyển dụng
+
+	public List<TuyenDung> getListReruitment() {
+		return listDao.getListRecruitment();
+	}
+	
+	
+	public List<TuyenDung> getReruitmentById(int id) {
+		return listDao.getRecruitmentById(id);
+	}
+	public Map<ChonDichVu, ChonDichVu> getListValueServiceOfDetailRecruitment() {
+		return listDao.getListValueServiceOfDetailRecruitment();
+	}
+
+	public int saveRegisterRecruitment(ChonDichVu dv, ListNhanVienvaDichVu hsnv, String filename) {
+		return listDao.saveRegisterRecruitment(dv,hsnv,filename);
+	}
+	
+	// đóng tuyên dụng
+	
+	public int saveContent(LienHe emp) {
+		return listDao.saveLienHe(emp);
+	}
+
+
+	
+
+	
+	
+
+	
+
+}
---
+++ ChamSocBenhNhan/src/main/java/ChamSocBenhNhan/userController/HomeUserController.java
@@ -0,0 +1,167 @@
+package ChamSocBenhNhan.userController;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import ChamSocBenhNhan.Entity.User.ChonDichVu;
+import ChamSocBenhNhan.Entity.User.LienHe;
+import ChamSocBenhNhan.Entity.User.ListNhanVienvaDichVu;
+import ChamSocBenhNhan.Service.user.HomeUserIml;
+
+@Controller
+public class HomeUserController extends BaseControlUser { 
+	@Autowired 
+	private HomeUserIml listhome = new HomeUserIml(); 
+
+	@RequestMapping(value = { "/home" }) 
+	public ModelAndView home(HttpSession ss) { 
+		_mvShare.addObject("nhanvienn", listhome.getListCardE());
+		ss.setAttribute("listdichvu", listhome.getListService()); 
+		ss.setAttribute("listtuyendung", listhome.getListReruitment()); 
+		_mvShare.addObject("totalRegisterService", listhome.getTotalRegisterService());
+		_mvShare.setViewName("user/index");
+		ss.removeAttribute("limit");
+		return _mvShare; 
+
+	} 
+	
+	@RequestMapping(value = { "/home/search" })
+	public ModelAndView searchService(String emp) {
+		if (listhome.searchNameService(emp)!=null) {
+			String message = "<script>alert('Gửi liên hệ thành công!!!');</script>";
+			try {
+				_mvShare.setViewName("redirect:/home/lienhe?message=" + URLEncoder.encode(message, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		} else {
+			String message = "<script>alert('Gửi liên hệ không thành công!!!');</script>";
+			try {
+				_mvShare.setViewName("redirect:/home/lienhe?message=" + URLEncoder.encode(message, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		return _mvShare;
+	}
+
+
+	@RequestMapping(value = { "/home/gioithieu" })
+	public ModelAndView gioithieu(HttpSession ss) {
+		_mvShare.setViewName("user/gioithieu");
+		ss.removeAttribute("limit");
+		return _mvShare;
+	}
+
+	@RequestMapping(value = { "/home/nhanvien" })
+	public ModelAndView nhanvien(HttpSession ss) {
+		_mvShare.addObject("nhanvienn", listhome.getListCardE());
+		_mvShare.setViewName("user/nhanvien");
+		ss.removeAttribute("limit");
+		return _mvShare;
+	}
+
+	// tuyên dụng
+	
+	@RequestMapping(value = { "/home/cttd/{maTuyenDung}" })
+	public String chitiettuyendung(HttpSession ss, @PathVariable int maTuyenDung, Model m) {
+		// _mvShare.addObject("cttdd", listhome.getListChiTietTuyenDung());
+		m.addAttribute("cttd", listhome.getReruitmentById(maTuyenDung));
+		m.addAttribute("chonGiaTriDichVu", listhome.getListValueServiceOfDetailRecruitment());
+		// _mvShare.addObject("chonDichVu",
+		// listhome.getListItemServiceOfDetailRecruitment());
+		m.addAttribute("command", new ListNhanVienvaDichVu());
+		ss.removeAttribute("limit");
+		return "user/chitiettuyendung";
+	}
+
+	@RequestMapping(value = "/home/cttd/save/{maTuyenDung}", method = RequestMethod.POST)
+	public String saveChiTietTuyenDung(@RequestParam(value = "profile") CommonsMultipartFile file, HttpSession s,
+			ChonDichVu dv, ListNhanVienvaDichVu hsnv, @PathVariable int maTuyenDung)
+			throws UnsupportedEncodingException {
+
+		byte[] data = file.getBytes();
+		String path = s.getServletContext().getRealPath("/") + "assets" + File.separator + "user" + File.separator
+				+ "images" + File.separator + "nhanvien" + File.separator + file.getOriginalFilename();
+		String fileName;
+		if (path != "") {
+			try {
+				FileOutputStream fos = new FileOutputStream(path);
+				fos.write(data);
+				fos.close();
+				System.out.println("file upload");
+				fileName = file.getOriginalFilename();
+				if (listhome.saveRegisterRecruitment(dv, hsnv, fileName) == 1) {
+					String message = "<script>alert('Chúc mừng bạn đã đăng ký tuyển dụng thành công!!!');</script>";
+
+					return "redirect:/home/cttd/" + maTuyenDung + "?message="
+							+ URLEncoder.encode(message, "UTF-8");
+				}else if ( listhome.saveRegisterRecruitment(dv, hsnv, fileName) == 5) {
+					String message = "<script>alert('Bạn đã đăng ký tuyển dụng không thành công, nhập số điện thoại phải từ 10-11 số !!!');</script>";
+
+					return "redirect:/home/cttd/" + maTuyenDung + "?message=" + URLEncoder.encode(message, "UTF-8");
+
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("upload eror");
+			}
+			System.out.println(path);
+			// return "success";
+
+		}
+
+		String message = "<script>alert('Bạn đã đăng ký tuyển dụng không thành công, có thể là do bạn chưa đủ 16 tuổi hoặc bạn chưa chọn ảnh!!!');</script>";
+
+		return "redirect:/home/cttd/" + maTuyenDung + "?message=" + URLEncoder.encode(message, "UTF-8");
+
+	}
+	
+	// đóng tuyển dụng
+	
+	@RequestMapping(value = { "/home/lienhe" })
+	public ModelAndView lienhe(HttpSession ss) {
+		ss.removeAttribute("limit");
+		_mvShare.addObject("command", new LienHe());
+		_mvShare.setViewName("user/lienhe");
+		return _mvShare;
+	}
+
+	@RequestMapping(value = { "/home/lienhe/save" })
+	public ModelAndView saveLienhe(LienHe emp) {
+		int kq = listhome.saveContent(emp);
+		if (kq != 0) {
+			String message = "<script>alert('Gửi liên hệ thành công!!!');</script>";
+			try {
+				_mvShare.setViewName("redirect:/home/lienhe?message=" + URLEncoder.encode(message, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		} else {
+			String message = "<script>alert('Gửi liên hệ không thành công!!!');</script>";
+			try {
+				_mvShare.setViewName("redirect:/home/lienhe?message=" + URLEncoder.encode(message, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		return _mvShare;
+	}
+
+}
